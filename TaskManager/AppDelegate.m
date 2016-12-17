429d589f02821fcc33c596421f7610766dedcac4
confiwang <confiwang@tencent.com>
2016-12-17 14:44:57 +0800

    feat(column): 每列展示不同信息

--- TaskManager/AppDelegate.m
+++ TaskManager/AppDelegate.m
@@ -34,10 +34,44 @@
 }
 
 -(NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row{
+    
     NSTableCellView *cell = (NSTableCellView *)[tableView makeViewWithIdentifier:@"SUNCell" owner:nil];
     
-    cell.imageView.image = nil;
-    cell.textField.stringValue = @"confiwang";
+    if (tableColumn == tableView.tableColumns[0]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"hello";
+    }
+    
+    if (tableColumn == tableView.tableColumns[1]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"confiwang";
+    }
+    
+    if (tableColumn == tableView.tableColumns[2]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"must";
+    }
+    
+    if (tableColumn == tableView.tableColumns[3]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"be";
+    }
+    
+    if (tableColumn == tableView.tableColumns[4]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"a";
+    }
+    
+    if (tableColumn == tableView.tableColumns[5]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"good";
+    }
+    
+    if (tableColumn == tableView.tableColumns[6]){
+        cell.imageView.image = nil;
+        cell.textField.stringValue = @"boy";
+    }
+    
     
     return cell;
 }
