7225fd946d805158f747ab77a5ebf4dd19e965d6
confiwang <confiwang@tencent.com>
2016-12-25 14:36:49 +0800

    feat(select): 点击单元格显示单元格内容

--- TaskManager/AppDelegate.m
+++ TaskManager/AppDelegate.m
@@ -39,40 +39,63 @@
     
     if (tableColumn == tableView.tableColumns[0]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"hello";
+        cell.textField.stringValue = [@"hello" stringByAppendingFormat:@"%ld0",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[1]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"confiwang";
+        cell.textField.stringValue = [@"confiwang" stringByAppendingFormat:@"%ld1",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[2]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"must";
+        cell.textField.stringValue = [@"must" stringByAppendingFormat:@"%ld2",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[3]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"be";
+        cell.textField.stringValue = [@"be" stringByAppendingFormat:@"%ld3",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[4]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"a";
+        cell.textField.stringValue = [@"a" stringByAppendingFormat:@"%ld4",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[5]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"good";
+        cell.textField.stringValue = [@"good" stringByAppendingFormat:@"%ld5",(long)row];
     }
     
     if (tableColumn == tableView.tableColumns[6]){
         cell.imageView.image = nil;
-        cell.textField.stringValue = @"boy";
+        cell.textField.stringValue = [@"boy" stringByAppendingFormat:@"%ld6",(long)row];
     }
     
     
     return cell;
 }
+
+- (BOOL)tableView:(NSTableView *)tableView shouldSelectRow:(NSInteger)row{
+
+    return NO;
+}
+
+-(BOOL)tableView:(NSTableView *)tableView shouldSelectTableColumn:(NSTableColumn *)tableColumn {
+    return NO;
+}
+
+- (BOOL)selectionShouldChangeInTableView:(NSTableView *)tableView {
+    
+    NSInteger row = [tableView clickedRow];
+    NSInteger column = [tableView clickedColumn];
+    NSLog(@"%ld,%ld", row, column);
+    
+    NSTableCellView *cellView = [tableView viewAtColumn:column row:row makeIfNecessary:YES];
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:cellView.textField.stringValue];
+    [alert runModal];
+    return YES;
+}
+
 @end
